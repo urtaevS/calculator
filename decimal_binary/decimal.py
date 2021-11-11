cec27cbe42feb6cf7efc522ffe8c319b089abe5c
Hexlet Learn <urtaev.s@gmail.com>
2021-11-11 14:00:52 +0300

    first start

---
+++ decimal_binary/decimal.py
@@ -0,0 +1,11 @@
+from prompt import integer
+'''Binary function'''
+
+def input_decimal():
+    number = integer('Input a number: ')
+
+def calculate_binary():
+    remainder = None
+    number = input_decimal()
+    while remainder = 0:
+
